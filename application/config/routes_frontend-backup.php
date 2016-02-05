5b4ff9c06264403c68e6efc550a838a0ed825561
Marc Besinal <marc.besinal@viiworks.com>
2016-02-05 09:17:42 +0800

    application/config/routes_frontend.php update on the pages

---
+++ application/config/routes_frontend-backup.php
@@ -0,0 +1,63 @@
+<?php  if (!defined("BASEPATH")) exit("No direct script access allowed");
+
+$route["treatment/medical/dermaroller"] = "pages/index";
+$route["treatment/medical/skinbiopsyexcisionalbiopsy"] = "pages/index";
+$route["treatment/medical/iontophoresis"] = "pages/index";
+$route["treatment/medical/electrocautery "] = "pages/index";
+$route["treatment/medical/moleremoval  "] = "pages/index";
+$route["treatment/medical/cystremoval"] = "pages/index";
+$route["treatment/medical/moleremoval  "] = "pages/index";
+$route["treatment/medical/skinanalysis"] = "pages/index";
+$route["bodyservices/bodyscrub"] = "pages/index";
+$route["treatment/bodyservices/shapesculpta"] = "pages/index";
+$route["treatment/bodyservices/backcleaning"] = "pages/index";
+$route["treatment/bodyservices/waxing"] = "pages/index";
+$route["treatment/bodyservices/bodysaltglow"] = "pages/index";
+$route["treatment/regularfacials/agedefyfacial"] = "pages/index";
+$route["treatment/regularfacials/classicfacial"] = "pages/index";
+$route["treatment/regularfacials/oilcontrolfacial"] = "pages/index";
+$route["treatment/regularfacials/realgentlemanfacial"] = "pages/index";
+$route["treatment/regularfacials/acnecontrolfacial"] = "pages/index";
+$route["treatment/advancedfacials/agedefyadvancedfacial"] = "pages/index";
+$route["treatment/advancedfacials/realgentlemanadvancedfacial"] = "pages/index";
+$route["treatment/advancedfacials/oilcontroladvncedfacial"] = "pages/index";
+$route["treatment/advancedfacials/acnecontroladvancedfacial"] = "pages/index";
+$route["treatment/laserrfandothers/refine"] = "pages/index";
+$route["treatment/laserrfandothers/nanorf"] = "pages/index";
+$route["treatment/laserrfandothers/skinfusion"] = "pages/index";
+$route["treatment/laserrfandothers/advancedrf"] = "pages/index";
+$route["treatment/laserrfandothers/smartpl"] = "pages/index";
+$route["treatment/laserrfandothers/shapesculptcream"] = "pages/index";
+$route["products/beautykits/flawlesswhiteningkit"] = "pages/index";
+$route["treatment/laserrfandothers/laserhairremoval"] = "pages/index";
+$route["treatment/advancedfacials/whiteadvancedfacial"] = "pages/index";
+$route["treatment/medical/peels"] = "pages/index";
+$route["treatment/medical/cosmelan"] = "pages/index";
+$route["treatment/medical/fractionalneedling"] = "pages/index";
+$route["treatment/medical"] = "pages/index";
+$route["treatment/bodyservices"] = "pages/index";
+$route["treatment/regularfacials"] = "pages/index";
+$route["treatment/microdermabrasion"] = "pages/index";
+$route["treatment/laserrfandothers"] = "pages/index";
+$route["products/beautykits"] = "pages/index";
+$route["products/individualproducts"] = "pages/index";
+$route["treatment/advancedfacials"] = "pages/index";
+$route[""] = "pages/index";
+$route["error"] = "pages/error";
+$route["faqs"] = "faqs/index";
+$route["faqs/(.*)"] = "faqs/$1";
+
+$route["news"] = "news/index";
+$route["news/(.*)"] = "news/$1";
+
+$route["testimonial"] = "testimonial/index";
+$route["testimonial/(.*)"] = "testimonial/$1";
+
+$route["nanopowerpeel"] = "pages/index";
+$route["treatment"] = "pages/index";
+$route["products"] = "pages/index";
+$route["about"] = "pages/index";
+$route["flawlessbestsellers"] = "pages/index";
+
+
+?>
